ec64b1a98edfc2a3d3765cbc5d46d559b0873214
Lcurents <laurentiusdika28@gmail.com>
2023-12-09 19:36:51 +0700

    Perbaikan Input

--- Print.py
+++ Print.py
@@ -1,3 +1,5 @@
-print("Hallo Namaku Dika")
-x=input("Masukkan nilai X=")
-print("Nilai X adalah "+str(x))
+nama=input("Siapa Nama Kamu?")
+umur=input("Berapa umur Kamu?")
+tinggi=input("Berapa tinggi badan Kamu?")
+
+print("Halo "+nama+",Dari data yang ada umurmu "+umur+" Tahun,dan tinggi badan Kamu adalah "+tinggi)
